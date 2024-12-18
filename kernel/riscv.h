508e8bccc9c1347ec813d87c29489706b1942b97
jack <1641869338@qq.com>
2024-12-18 23:06:33 +0800

    print pagetable

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -329,12 +329,13 @@ sfence_vma()
 #define PTE_V (1L << 0) // valid
 #define PTE_R (1L << 1)
 #define PTE_W (1L << 2)
-#define PTE_X (1L << 3)
+#define PTE_X (1L << 3) 
 #define PTE_U (1L << 4) // 1 -> user can access
 
 // shift a physical address to the right place for a PTE.
 #define PA2PTE(pa) ((((uint64)pa) >> 12) << 10)
 
+// 获取指向的下一级
 #define PTE2PA(pte) (((pte) >> 10) << 12)
 
 #define PTE_FLAGS(pte) ((pte) & 0x3FF)
